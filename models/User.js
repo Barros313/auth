594990d3a47f9dc2744c69df078bcca256cbc78d
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-11-23 10:05:46 -0300

    Added user authentication model
    Signed-off-by: Gabriel Barros de Almeida <gbarros1337@gmail.com>

---
+++ models/User.js
@@ -0,0 +1,9 @@
+import mongoose, { mongo } from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+    name: { type: String, require: true },
+    email: { type: String, require: true, unique: true },
+    password: { type: String, require: true },
+});
+
+module.exports = mongoose.model('User', UserSchema);
