0a479390481734da7e6090facd8515ae041552ed
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-11-23 11:16:33 -0300

    Added login route
    Signed-off-by: Gabriel Barros de Almeida <gbarros1337@gmail.com>

--- routes/auth.js
+++ routes/auth.js
@@ -25,7 +25,32 @@ router.post('/register', async (req, res) => {
         return res.status(200).json({ message: `User registered successfully` });
     } catch (err) {
         res.status(500).json({ message: 'Internal server error' });
-        console.err(err);
+        console.error(err);
+    }
+});
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            console.log(`${email} not registered`);
+            return res.status(400).json({ message: 'User not found' });
+        }
+
+        const isMatch = password === user.password;
+        if (!isMatch) {
+            console.log('Passwords don\'t match');
+            return res.status(401).json({ message: 'Wrong credentials' });
+        }
+
+        console.log(`User ${user.name} (${user.email}) logged in`);
+        res.status(200).json({ user: { id: user._id, name: user.name, email: user.email } });
+
+    } catch(err) {
+        console.error(err)
+        res.status(500).json({ message: `Internal server error` });
     }
 });
 
