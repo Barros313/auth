19a5c4c3250e2eee3694b9e684c44b77d6c99a48
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-11-23 10:31:21 -0300

    Built signin route
    Signed-off-by: Gabriel Barros de Almeida <gbarros1337@gmail.com>

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,32 @@
 import express from 'express';
+import User from '../models/User.js';
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
     res.send("Hello World!");
+});
+
+router.post('/register', async (req, res) => {
+    const { name, email, password } = req.body;
+
+    try {
+        const registeredUser = await User.findOne({ email });
+        if (registeredUser) {
+            console.log(`${email} already registered`);
+            
+            return res.status(401).json({ message: 'User already exists' });
+        } 
+
+        const newUser = new User({ name: name, email: email, password: password });
+        await newUser.save();
+
+        console.log(`User ${name} registered successfully`);
+        return res.status(200).json({ message: `User registered successfully` });
+    } catch (err) {
+        res.status(500).json({ message: 'Internal server error' });
+        console.err(err);
+    }
 });
 
 export default router;
