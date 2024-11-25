366339929d20cdb3a7b484e28e3aa46adeec9c21
Barros313 <gbarros1337@gmail.com>
2024-11-25 16:28:08 -0300

    Added brute force prevention on auth controller
    Signed-off-by: Barros313 <gbarros1337@gmail.com>

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,22 @@ import User from '../models/User.js';
 
 const router = express.Router();
 
+const MAX_LOGIN_ATTEMPTS = 3;
+const LOCK_TIME = 2*60*60*1000;
+
+const formatDate = (date) => {
+    const pad = (num) => String(num).padStart(2, '0');
+
+    const year = date.get;
+    const month = pad(date.getMonth() + 1); // Months are zero-based
+    const day = pad(date.getDate());
+    const hours = pad(date.getHours());
+    const minutes = pad(date.getMinutes());
+    const seconds = pad(date.getSeconds());
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+};
+
 router.post('/register', async (req, res) => {
     const { name, email, password } = req.body;
 
@@ -30,31 +46,41 @@ router.post('/login', async (req, res) => {
 
     try {
         const user = await User.findOne({ email });
+
+        await user.resetLock();
+
         if (!user) {
             console.log(`${email} not registered`);
             return res.status(400).json({ message: 'User not found' });
         }
 
-        User.getAuthenticated(email, password, function (error, user, reason) {
-            if (error) throw error;
+        if (user.isLocked) {
+            console.log(`${email} is locked out until ${user.lockUntil}`);
+            return res.status(403).json({ message: 'Account is temporarily locked. Try again later' });
+        }
 
-            if (user) {
-                console.log(`${email} logged in`);
-                return res.status(200).json({ message: 'Success!' });
+        const isMatch = await user.comparePassword(password);
+
+        if (isMatch) {
+            user.loginAttempts = 0;
+            user.lockUntil = null;
+            await user.save();
+
+            console.log(`User ${user.name} (${user.email}) logged in`);
+            res.status(200).json({ user: { id: user._id, name:user.name, email: user.email } });
+        } else {
+            user.loginAttempts += 1;
+
+            if (user.loginAttempts >= 3) {
+                user.lockUntil = new Date(Date.now() + 2*60*60*1000);
+                console.log(`User ${user.email} is now locked out.`);
             }
 
-            let reasons = User.failedLogin;
+            await user.save();
 
-            switch (reason) {
-                case reasons.NOT_FOUND:
-                    break;
-                case reasons.PASSWORD_INCORRECT:
-                    break;
-                case reasons.MAX_ATTEMPTS:
-                    break;
-            }
-        });
-
+            console.log(`${email} login failed. Passwords don't match`);
+            return res.status(401).json({ message: 'Wrong credentials' });
+        }
     } catch(err) {
         console.error(err)
         res.status(500).json({ message: `Internal server error` });
