0b359101b69fd6aca8053d1bda346ea2fe6aff66
Barros313 <gbarros1337@gmail.com>
2024-11-25 19:31:41 -0300

    Added strong password validator
    Signed-off-by: Barros313 <gbarros1337@gmail.com>

--- models/User.js
+++ models/User.js
@@ -7,7 +7,16 @@ const UserSchema = new mongoose.Schema(
     {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: { 
+        type: String, 
+        required: true,
+        validate: {
+            validator: function (value) {
+                return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(value);
+            },
+            message: 'Password must be at least 8 characters long and contain at least one letter, one number and one special character'
+        } 
+    },
     loginAttempts: { type: Number, required: true, default: 0 },
     lockUntil: { type: Number }
     },
