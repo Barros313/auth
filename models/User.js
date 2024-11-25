6ac165c59984c90b1791d8853bab92982910d939
Barros313 <gbarros1337@gmail.com>
2024-11-25 16:27:54 -0300

    Removed prevent multiple attempts logic from User model
    Signed-off-by: Barros313 <gbarros1337@gmail.com>

--- models/User.js
+++ models/User.js
@@ -5,117 +5,52 @@ const SALT_WORK_FACTOR = process.env.SALT_WORK_FACTOR;
 const MAX_LOGIN_ATTEMPTS = process.env.MAX_LOGIN_ATTEMPTS;
 const LOCK_TIME = process.env.LOCK_TIME;
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema(
+    {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     loginAttempts: { type: Number, required: true, default: 0 },
     lockUntil: { type: Number }
-});
+    },
+    { timestamps: true }
+);
 
 UserSchema.virtual('isLocked').get(function () {
     return !!(this.lockUntil && this.lockUntil > Date.now());
 });
 
-UserSchema.pre('save', async function (next) { 
+UserSchema.pre('save', function (next) { 
     const user = this;
 
     if (!user.isModified('password')) return next();
 
-    try {
-        const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
-        user.password = await bcrypt.hash(user.password, salt);
-        next();
-    } catch (error) {
-        next(error);
-    }
-
-    /* bcrypt.genSalt(SALT_WORK_FACTOR, (error, salt) => {
+    bcrypt.genSalt(SALT_WORK_FACTOR, (error, salt) => {
         if (error) return next(error);
 
-        // Encripta senha com "sal" gerado
         bcrypt.hash(user.password, salt, (error, hash) => {
             if (error) return next(error);
 
-            // Substitui plaintext
             user.password = hash;
             next();
         });
-    }); */
+    });
 });
 
-UserSchema.methods.comparePassword = function (inputPassword, callback) {
-    bcrypt.compare(inputPassword, this.password, (err, isMatch) => {
-        if (err) return callback(err);
-
-        callback(null, isMatch);
-    });
-};
-
-UserSchema.methods.incLoginAttempts = function(callback) {
-    if (this.lockUntil && this.lockUntil < Date.now()) {
-        return this.update({ 
-            $set: { loginAttempts: 1 }, 
-            $unset: {lockUntil: 1 } }, 
-            callback);
+UserSchema.methods.comparePassword = async function (inputPassword) {
+    try {
+        return await bcrypt.compare(inputPassword, this.password);
+    } catch (error) {
+        throw error;
     }
-
-    let updates = { $inc: { loginAttempts: 1 } };
-
-    if (this.loginAttempts + 1 >= MAX_LOGIN_ATTEMPTS && !this.isLocked) {
-        updates.$set = { lockUntil: Date.now() + LOCK_TIME };
-    } 
-    
-    return this.update(updates, callback);
 };
 
-const reasons = UserSchema.statics.failedLogin = {
-    NOT_FOUND: 0,
-    PASSWORD_INCORRECT: 1,
-    MAX_ATTEMPTS: 2
-};
-
-UserSchema.statics.getAuthenticated = function(email, password, callback) {
-    this.findOne({ email: email }, function (error, user) {
-        if (error) return callback(error);
-
-        if (!user) {
-            return callback(null, null, reasons.NOT_FOUND);
-        }
-
-        if (user.isLocked) {
-            return user.incLoginAttempts(function(error) {
-                if (error) return callback(error);
-
-                return callback(null, null, reasons.MAX_ATTEMPTS);
-            });
-        }
-
-        user.comparePassword(password, function (error, isMatch) {
-            if (error) return callback(error);
-
-            if (isMatch) {
-                if (!user.loginAttempts && !user.lockUntil) return callback(null, user);
-
-                let updates = {
-                    $set: { loginAttempts: 1 },
-                    $unset: { lockUntil: 1 }
-                };
-
-                return user.update(updates, function (error) {
-                    if (error) return callback(error);
-
-                    return callback(null, user);
-                });
-            }
-
-            user.incLoginAttempts(function(error) {
-                if (error) return callback(error);
-
-                return callback(null, null, reasons.PASSWORD_INCORRECT);
-            })
-        })
-    })
+UserSchema.methods.resetLock = async function () {
+    if (this.lockUntil && this.lockUntil <= Date.now()) {
+        this.loginAttempts = 0;
+        this.lockUntil = null;
+        await this.save();
+    }
 }
 
 export default mongoose.model('User', UserSchema);
