40aee96ef5748ec4fdeb3d0a4368a98d9eb13eff
Barros313 <gbarros1337@gmail.com>
2024-11-25 09:22:51 -0300

    Replaced compare function to new user method
    Signed-off-by: Barros313 <gbarros1337@gmail.com>

--- routes/auth.js
+++ routes/auth.js
@@ -35,15 +35,17 @@ router.post('/login', async (req, res) => {
             return res.status(400).json({ message: 'User not found' });
         }
 
-        const isMatch = password === user.password;
-        if (!isMatch) {
-            console.log('Passwords don\'t match');
-            return res.status(401).json({ message: 'Wrong credentials' });
-        }
+        user.comparePassword(password, (err, isMatch) => {
+            if (err) throw err;
 
-        console.log(`User ${user.name} (${user.email}) logged in`);
-        res.status(200).json({ user: { id: user._id, name: user.name, email: user.email } });
-
+            if (isMatch) {
+                console.log(`User ${user.name} (${user.email}) logged in`);
+                res.status(200).json({ user: { id: user._id, name: user.name, email: user.email } });
+            } else {
+                console.log(`${email} login failed. Passwords don't match`);
+                return res.status(401).json({ message: 'Wrong credentials' });                
+            }
+        });
     } catch(err) {
         console.error(err)
         res.status(500).json({ message: `Internal server error` });
