89313591abcf6e9fef2b39e8149dc8e585367c6c
Barros313 <gbarros1337@gmail.com>
2024-11-25 09:21:05 -0300

    Added cryptograph to user schema and method to compare passwords
    Signed-off-by: Barros313 <gbarros1337@gmail.com>

--- models/User.js
+++ models/User.js
@@ -1,9 +1,39 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const SALT_WORK_FACTOR = process.env.SALT_WORK_FACTOR;
 
 const UserSchema = new mongoose.Schema({
     name: { type: String, require: true },
     email: { type: String, require: true, unique: true },
     password: { type: String, require: true },
 });
+
+UserSchema.pre('save', function (next) { 
+    const user = this;
+
+    if (!user.isModified('password')) return next();
+
+    bcrypt.genSalt(SALT_WORK_FACTOR, (error, salt) => {
+        if (error) return next(error);
+
+        // Encripta senha com "sal" gerado
+        bcrypt.hash(user.password, salt, (error, hash) => {
+            if (error) return next(error);
+
+            // Substitui plaintext
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+UserSchema.methods.comparePassword = function (inputPassword, callback) {
+    bcrypt.compare(inputPassword, this.password, (err, isMatch) => {
+        if (err) return callback(err);
+
+        callback(null, isMatch);
+    });
+}
 
 export default mongoose.model('User', UserSchema);
